ec954e48c2d1016ae206cdda4d57838464e11f83
nabbisen <nabbisen@scqr.net>
2026-01-07 07:12:57 +0000

    child component send req to open common dialog; card info demo (#9)
    * card info as git status digest
    
    * child component send req to open common dialog

--- src/app/views/dashboard.rs
+++ src/app/views/dashboard.rs
@@ -1,23 +1,31 @@
+use std::{path::Path, rc::Rc};
+
 use relm4::factory::FactoryVecDeque;
 use relm4::gtk::prelude::*;
 use relm4::prelude::*;
 
-use crate::app::components::dashboard::card::{Card, CardModel};
+use crate::app::components::dashboard::card::{Card, CardModel, CardOutput};
 
 pub struct DashboardModel {
     pub cards: FactoryVecDeque<Card>,
 }
 
 #[derive(Debug)]
-pub enum DashboardMsg {
-    Randomize,
+pub enum DashboardInput {
+    Demo,
+    RepoSelected,
+}
+
+#[derive(Debug)]
+pub enum DashboardOutput {
+    RepoSelected,
 }
 
 #[relm4::component(pub)]
 impl Component for DashboardModel {
     type Init = ();
-    type Input = DashboardMsg;
-    type Output = ();
+    type Input = DashboardInput;
+    type Output = DashboardOutput;
     type CommandOutput = ();
 
     view! {
@@ -26,8 +34,8 @@ impl Component for DashboardModel {
             set_spacing: 10,
 
             gtk::Button {
-                set_label: "ランダム生成",
-                connect_clicked => DashboardMsg::Randomize,
+                set_label: "デモ生成",
+                connect_clicked => DashboardInput::Demo,
             },
 
             gtk::ScrolledWindow {
@@ -38,13 +46,15 @@ impl Component for DashboardModel {
         }
     }
 
-    fn init(_init: Self::Init, _root: Self::Root, sender: ComponentSender<Self>) -> ComponentParts<Self> {
+    fn init(
+        _init: Self::Init,
+        _root: Self::Root,
+        sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
         let cards = FactoryVecDeque::builder()
             .launch(gtk::Box::new(gtk::Orientation::Vertical, 5))
-            .forward(sender.input_sender(), |_| {
-                // 子コンポーネントの Output が () の場合は、
-                // 親の Input に変換するメッセージがないため何もしない
-                unreachable!() 
+            .forward(sender.input_sender(), |msg| match msg {
+                CardOutput::RepoSelected => DashboardInput::RepoSelected,
             });
         let model = DashboardModel { cards };
         let card_box = model.cards.widget();
@@ -53,15 +63,18 @@ impl Component for DashboardModel {
         ComponentParts { model, widgets }
     }
 
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>, _root: &Self::Root) {
+    fn update(&mut self, msg: Self::Input, sender: ComponentSender<Self>, _root: &Self::Root) {
         match msg {
-            DashboardMsg::Randomize => {
+            DashboardInput::Demo => {
                 let mut guard = self.cards.guard();
                 guard.clear();
-                for i in 1..=10 {
-                    guard.push_back(CardModel { label: format!("Card #{}", i) });
+                for _i in 1..=3 {
+                    guard.push_back(CardModel {
+                        path: Rc::from(Path::new(".")),
+                    });
                 }
             }
+            DashboardInput::RepoSelected => sender.output(DashboardOutput::RepoSelected).unwrap(),
         }
     }
 }
--- src/app/window.rs
+++ src/app/window.rs
@@ -1,18 +1,28 @@
 use gtk::prelude::{BoxExt, GtkWindowExt};
 use relm4::gtk::prelude::*;
-use relm4::{Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmWidgetExt, SimpleComponent, gtk};
+use relm4::{
+    Component, ComponentController, ComponentParts, ComponentSender, Controller, RelmWidgetExt,
+    SimpleComponent, gtk,
+};
 
-use crate::app::views::dashboard::DashboardModel;
+use crate::app::components::common::dialog::{DialogInput, DialogModel, DialogOutput};
+use crate::app::views::dashboard::{DashboardModel, DashboardOutput};
 
 pub struct AppModel {
     dashboard: Controller<DashboardModel>,
+    dialog: Controller<DialogModel>,
+}
+
+#[derive(Debug)]
+pub enum AppInput {
+    RepoSelected,
 }
 
 #[relm4::component(pub)]
 impl SimpleComponent for AppModel {
     type Init = u8;
 
-    type Input = ();
+    type Input = AppInput;
     type Output = ();
 
     view! {
@@ -25,7 +35,7 @@ impl SimpleComponent for AppModel {
                 set_orientation: gtk::Orientation::Vertical,
                 set_spacing: 5,
                 set_margin_all: 5,
-                
+
                 #[local_ref]
                 dashboard_widget -> gtk::Box {}
             }
@@ -35,13 +45,24 @@ impl SimpleComponent for AppModel {
     fn init(
         _init: Self::Init,
         root: Self::Root,
-        _sender: ComponentSender<Self>,
+        sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
-        let dashboard = DashboardModel::builder()
-            .launch(())
+        let dashboard =
+            DashboardModel::builder()
+                .launch(())
+                .forward(sender.input_sender(), |msg| match msg {
+                    DashboardOutput::RepoSelected => AppInput::RepoSelected,
+                });
+
+        let dialog = DialogModel::builder()
+            .transient_for(&root)
+            .launch(true)
+            // .forward(sender.input_sender(), |msg| match msg {
+            //     _ => (),
+            // })
             .detach();
 
-        let model = AppModel { dashboard };
+        let model = AppModel { dashboard, dialog };
 
         // Insert the macro code generation here
         let dashboard_widget = model.dashboard.widget();
@@ -50,5 +71,11 @@ impl SimpleComponent for AppModel {
         ComponentParts { model, widgets }
     }
 
-    // fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) { }
+    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {
+        match msg {
+            AppInput::RepoSelected => {
+                self.dialog.sender().send(DialogInput::Show).unwrap();
+            }
+        }
+    }
 }
